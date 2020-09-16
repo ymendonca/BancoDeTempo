7fc733d975e60bcbefb1fcc0e9e44b8dbf7277aa
yanick mendonça <janick0605@hotmail.com>
2020-09-16 02:43:58 -0100

    servicos criados e servicos adcionado a memoria

---
+++ BancoDeTempo/BancoDeTempo.Core/Models/servicos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BancoDeTempo.Core.Models
+{
+   public class Servicos
+    {
+        public string Id { get; set; }
+
+        //[StringLength(40)]
+        [DisplayName ("Titulo Experiencia")]
+        public string titulo { get; set; }
+        public string Descrisao { get; set; }
+        public string Imagem { get; set; }
+        public DateTime duracao { get; set; }
+        public string linguagem_partilha { get; set; }
+        public string experiencia_assunto { get; set; }
+        public string disponibilidade_partilha { get; set; }
+        public string condicao_acesso { get; set; }
+
+        public Servicos()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+        
+
+    }
+}
---
+++ BancoDeTempo/BancoDeTempo.DataAccess.InMemories/ServicosRepositorio.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using BancoDeTempo.Core.Models;
+
+namespace BancoDeTempo.DataAccess.InMemories
+{
+    public class ServicosRepositorio
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Servicos> servicos;
+
+        public ServicosRepositorio()
+        {
+            servicos = cache["servicos"] as List<Servicos>;
+            if(servicos == null)
+            {
+                servicos = new List<Servicos>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["servicos"] = servicos; 
+        }
+
+        public void Insert(Servicos s)
+        {
+            servicos.Add(s);
+        }
+
+        public void Update(Servicos servico)
+        {
+            Servicos servicoToUpdate = servicos.Find(s => s.Id == servico.Id);
+
+            if(servicoToUpdate != null) {
+                servicoToUpdate = servico;
+            }
+            else
+            {
+                throw new Exception("Servicos nao encontrado");
+            }
+        }
+
+        public Servicos Find(string Id)
+        {
+            Servicos servico = servicos.Find(s => s.Id == Id);
+
+            if (servico != null)
+            {
+                return servico;
+            }
+            else
+            {
+                throw new Exception("Servicos nao encontrado");
+            }
+        }
+
+        public IQueryable<Servicos> Collection()
+        {
+            return servicos.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Servicos servicoToDelete = servicos.Find(s => s.Id == Id);
+
+            if (servicoToDelete != null)
+            {
+                servicos.Remove(servicoToDelete);
+            }
+            else
+            {
+                throw new Exception("Servicos nao encontrado");
+            }
+        }
+
+    }
+}
